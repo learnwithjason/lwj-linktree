ed0407b767f56ae63e2318113a5f61772624afaf
Jason Lengstorf <jason@lengstorf.com>
2022-09-08 10:49:35 -0700

    feat: display Twitch stats

---
+++ netlify/edge-functions/add-stats.ts
@@ -0,0 +1,32 @@
+import type { Context } from 'https://edge.netlify.com';
+import type { Element } from 'https://ghuc.cc/worker-tools/html-rewriter/index.ts';
+import { HTMLRewriter } from 'https://ghuc.cc/worker-tools/html-rewriter/index.ts';
+
+export default async function (_request: Request, context: Context) {
+  const response = await context.next();
+  const stats = await fetch(
+    'http://localhost:8888/.netlify/functions/get-twitch-stats',
+  );
+  const twitch = await stats.json();
+
+  const formatted = new Intl.NumberFormat('en-US', {
+    notation: 'compact',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 1,
+  }).format(twitch.stats.followers);
+
+  return new HTMLRewriter()
+    .on('[data-site="twitch"]', {
+      element(element: Element) {
+        element.append(
+          `
+          <span class="stats">${formatted} followers</span>
+        `,
+          {
+            html: true,
+          },
+        );
+      },
+    })
+    .transform(response);
+}
--- src/components/links.tsx
+++ src/components/links.tsx
@@ -2,24 +2,29 @@ import styles from './links.module.css';
 
 type Link = {
   label: string;
+  slug: string;
   url: string;
 };
 
 const LINKS: Link[] = [
   {
     label: 'Twitter',
+    slug: 'twitter',
     url: 'https://twitter.com/LWJShow',
   },
   {
     label: 'Learn With Jason',
+    slug: 'learnwithjason',
     url: 'https://www.learnwithjason.dev',
   },
   {
     label: 'YouTube',
+    slug: 'youtube',
     url: 'https://www.youtube.com/channel/UCnty0z0pNRDgnuoirYXnC5A',
   },
   {
     label: 'Twitch',
+    slug: 'twitch',
     url: 'https://twitch.com/jlengstorf',
   },
 ];
@@ -28,7 +33,12 @@ export function Links() {
   return (
     <nav className={styles.links}>
       {LINKS.map((link) => (
-        <a key={link.url} href={link.url} className={styles.link}>
+        <a
+          key={link.url}
+          href={link.url}
+          className={styles.link}
+          data-site={link.slug}
+        >
           {link.label}
         </a>
       ))}
